b53b39ebe82f167013da86557c3074a75cc1722c
sadeghhosseiny <thesadegh.h@gmail.com>
2022-06-29 22:47:59 +0430

    create a cart for every post in post page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import { request } from "../services/apiRequest";
 import { wrapper } from "../store";
+import { getPosts } from "../store/actions";
 
 export default function Home() {
   const data = useSelector((state) => state);
--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,9 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { baseUrl } from "../../constants/baseUrl";
+import {
+  Card,
+  CardHeader,
+  Typography,
+  CardActions,
+  CardContent,
+  Collapse,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import { Box } from "@mui/system";
 
 function Post({ post }) {
-  return <div></div>;
+  const [expand, setExpand] = useState(false);
+
+  const handleClickExpand = () => {
+    setExpand(!expand);
+  };
+
+  return (
+    <Box
+      sx={{ width: "inherit", height: "inherit" }}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Box width="445px" height="345px">
+        <Card
+          sx={{
+            maxWidth: 445,
+            boxShadow: "2px 2px 3px #acacad",
+            ":hover": {
+              boxShadow: "8px 5px 5px #acacad",
+            },
+          }}
+        >
+          <CardHeader
+            avatar={
+              <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
+                {post.id}
+              </Avatar>
+            }
+            title={post.title}
+            subheader="September 14, 2016"
+          />
+          <CardActions sx={{ justifyContent: "end" }} disableSpacing>
+            <IconButton onClick={handleClickExpand}>
+              <ExpandMoreIcon
+                sx={
+                  expand
+                    ? {
+                        transform: "rotate(180deg)",
+                        transition: "0.3s",
+                      }
+                    : {
+                        transform: "rotate(0deg)",
+                        transition: "0.3s",
+                      }
+                }
+              />
+            </IconButton>
+          </CardActions>
+          <Collapse in={expand} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph>{post.body}</Typography>
+            </CardContent>
+          </Collapse>
+        </Card>
+      </Box>
+    </Box>
+  );
 }
 
 export async function getServerSideProps(context) {
